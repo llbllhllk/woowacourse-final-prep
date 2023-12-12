b399364e9fc4e406c45b6096c2309dd5ac823988
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2023-12-12 10:17:42 +0900

    feat: 시도 횟수를 입력 받는 기능 추가

--- javascript-racingcar-6-main/src/controller/RacingGameController.js
+++ javascript-racingcar-6-main/src/controller/RacingGameController.js
@@ -14,7 +14,14 @@ class RacingGameController {
 
   async #inputCarNames() {
     const carNames = await this.#inputView.readCarNames();
-    console.log(carNames);
+
+    this.#inputAttempts();
+  }
+
+  async #inputAttempts() {
+    const attempts = await this.#inputView.readAttempts();
+
+    console.log(attempts);
   }
 }
 
--- javascript-racingcar-6-main/src/view/InputView.js
+++ javascript-racingcar-6-main/src/view/InputView.js
@@ -8,6 +8,12 @@ const InputView = {
 
     return carNames;
   },
+
+  async readAttempts() {
+    const attempts = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+
+    return attempts;
+  },
 };
 
 export default InputView;
