2f469b652d5f2c280af470306193df54f942a3e2
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2023-12-12 18:36:38 +0900

    feat: 구매 개수 만큼의 로또 번호를 생성하는 기능 추가

--- javascript-lotto-6-main/src/controller/LottoController.js
+++ javascript-lotto-6-main/src/controller/LottoController.js
@@ -1,4 +1,6 @@
+import { Random } from '@woowacourse/mission-utils';
 import BonusNumber from '../domain/BonusNumber.js';
+import Lotto from '../domain/Lotto.js';
 import Purchase from '../domain/Purchase.js';
 import WinningNumbers from '../domain/WinningNumbers.js';
 
@@ -30,8 +32,26 @@ class LottoController {
   #printNumberOfPurchase(formattedAmount) {
     const numberOfPurchase = formattedAmount / 1000;
     this.#outputView.printNumberOfPurchase(numberOfPurchase);
+
+    return this.#printLottos(numberOfPurchase);
+  }
+
+  #printLottos(numberOfPurchase) {
+    const lottosString = Array.from({ length: numberOfPurchase }, () => {
+      const lottoNumber = this.#generateLottoNumber();
+      return new Lotto(lottoNumber).lottoString();
+    });
     
     return this.#inputWinningNumbers();
+  }
+
+  #generateLottoNumber() {
+    const randomNumbers = Random.pickUniqueNumbersInRange(1, 45, 6);
+    return this.#sortNumbers(randomNumbers);
+  }
+
+  #sortNumbers(numbers) {
+    return numbers.sort((a, b) => a - b);
   }
 
   async #inputWinningNumbers() {
--- javascript-lotto-6-main/src/domain/Lotto.js
+++ javascript-lotto-6-main/src/domain/Lotto.js
@@ -6,10 +6,19 @@ class Lotto {
     this.#numbers = numbers;
   }
 
+  lottoString() {
+    return `[${this.#numbers.join(', ')}]`;
+  }
+
   #validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
+
+    numbers.forEach(number => {
+      if (number < 1 || number > 45)
+        throw new Error('[ERROR] 로또 번호는 1~45의 숫자 범위를 가집니다.');
+    });
   }
 }
 
