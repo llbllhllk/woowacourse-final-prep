5c6121370d49d3c563d5b4bf7f44c165ab21de55
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2023-12-12 11:02:00 +0900

    feat: 최종 우승자를 반환하는 기능 추가

--- javascript-racingcar-6-main/src/controller/RacingGameController.js
+++ javascript-racingcar-6-main/src/controller/RacingGameController.js
@@ -27,10 +27,10 @@ class RacingGameController {
     const attempts = await this.#inputView.readAttempts();
     const formattedAttempts = new Attempts(attempts).getFormattedAttempts();
 
-    return this.printResult(formattedCarNames, formattedAttempts);
+    return this.#printResult(formattedCarNames, formattedAttempts);
   }
 
-  printResult(formattedCarNames, formattedAttempts) {
+  #printResult(formattedCarNames, formattedAttempts) {
     this.#outputView.printResultHeaderString();
     const cars = formattedCarNames.map(name => new Car(name));
     Array.from({ length: formattedAttempts }, () => {
@@ -38,6 +38,18 @@ class RacingGameController {
       const statusString = cars.map(car => car.statusString());
       this.#outputView.printStatusString(statusString);
     });
+
+    return this.#printWinners(cars);
+  }
+
+  #printWinners(cars) {
+    const carStatus = cars.map(car => car.statusString().split(' : '));
+    const max = Math.max(...cars.map(car => car.statusString().split(' : ')[1].length));
+    const winners = carStatus
+      .map(car => {
+        if (car[1].length >= max) return car[0];
+      })
+      .filter(Boolean);
   }
 }
 
