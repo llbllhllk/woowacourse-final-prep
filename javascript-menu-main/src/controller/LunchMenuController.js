bd0b126fe5b571b37b17d98399bd8acc37d74bb8
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2023-12-13 16:01:16 +0900

    init: MVC 아키텍처 구조 구성

---
+++ javascript-menu-main/src/controller/LunchMenuController.js
@@ -0,0 +1,18 @@
+class LunchMenuController {
+  // #service;
+
+  #inputView;
+
+  #outputView;
+
+  constructor(inputView, outputView) {
+    // param: service
+    // this.#service = service;
+    this.#inputView = inputView;
+    this.#outputView = outputView;
+  }
+
+  start() {}
+}
+
+export default LunchMenuController;
---
+++ javascript-menu-main/src/view/OutputView.js
@@ -0,0 +1,11 @@
+
+import { Console } from '@woowacourse/mission-utils';
+import MESSAGE from '../constants/message.js';
+
+const OutputView = {
+  printString() {
+    Console.print();
+  },
+};
+
+export default OutputView;
