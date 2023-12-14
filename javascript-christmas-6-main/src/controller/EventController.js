621b32e3b6ce134d03dc96ce7e2df91f54766873
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2023-12-14 18:11:19 +0900

    feat: 이벤트 안내를 출력하는 기능 추가

--- javascript-christmas-6-main/src/controller/EventController.js
+++ javascript-christmas-6-main/src/controller/EventController.js
@@ -14,6 +14,7 @@ class EventController {
   }
 
   start() {
+    this.#outputView.printGreetingString();
     return this.#inputVisitDate();
   }
 
--- javascript-christmas-6-main/src/view/OutputView.js
+++ javascript-christmas-6-main/src/view/OutputView.js
@@ -4,6 +4,10 @@ const OutputView = {
   printString() {
     Console.print();
   },
+
+  printGreetingString() {
+    Console.print('안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.');
+  },
 };
 
 export default OutputView;
