5882a39b38a8b100f148491abc4db7046b490d49
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2023-12-14 20:41:30 +0900

    feat: 12월 이벤트 배지를 출력하는 기능 추가

--- javascript-christmas-6-main/src/view/OutputView.js
+++ javascript-christmas-6-main/src/view/OutputView.js
@@ -75,6 +75,11 @@ const OutputView = {
     Console.print('\n<할인 후 예상 결제 금액>');
     Console.print(expectDiscountString);
   },
+
+  printEventBadgeString(eventBadge) {
+    Console.print('\n<12월 이벤트 배지>');
+    Console.print(eventBadge);
+  },
 };
 
 export default OutputView;
