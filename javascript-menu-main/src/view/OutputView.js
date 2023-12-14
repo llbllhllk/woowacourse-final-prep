e27c6ff3eb029e58f293bbdff7ebe67234d4fceb
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2023-12-14 14:07:14 +0900

    feat: 메뉴 추천 결과 안내문을 출력하는 기능 추가

--- javascript-menu-main/src/view/OutputView.js
+++ javascript-menu-main/src/view/OutputView.js
@@ -4,6 +4,10 @@ const OutputView = {
   printStartString() {
     Console.print('점심 메뉴 추천을 시작합니다.\n');
   },
+
+  printResultHeaderString() {
+    Console.print('메뉴 추천 결과입니다.');
+  },
 };
 
 export default OutputView;
